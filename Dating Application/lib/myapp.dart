990780332504e2ff9c58a3a8b164dae89d63cf66
swat1998 <saswatnayak1998@gmail.com>
2020-06-11 00:18:44 +0530

    initial app commit v0.1

---
+++ Dating Application/lib/myapp.dart
@@ -0,0 +1,14 @@
+import 'package:date/numberpage.dart';
+import 'package:flutter/material.dart';
+import 'loginpage.dart';
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: numberInput(),
+    );
+  }
+}
---
+++ Dating Application/lib/numberpage.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class numberInput extends StatefulWidget {
+  @override
+  _numberInputState createState() => _numberInputState();
+}
+
+class _numberInputState extends State<numberInput> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xFF303030),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  color: Color(0xFFFE3C72),
+//                onPressed: Navigator.pop(context),// to add routing later
+                )
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text("My number is", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 30, color: Colors.white), textAlign: TextAlign.left,),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  child: Center()
+                ),
+                SizedBox(height: 20),
+                Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50.0),
+                          color: Color(0xFFFE3C72)
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text("CONTINUE", style: TextStyle(fontWeight: FontWeight.w500, color: Colors.white,))
+                        ],
+                      ),
+                    ),
+                    onTap: (){
+                      //new page route
+                    },
+                  ),
+                ),
+                SizedBox(height: 20,)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
